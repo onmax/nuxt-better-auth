b98e191fda5884104348047b9b28675ea2b8110b
onmax <maximogarciamtnez@gmail.com>
2025-12-17 11:11:00 +0100

    fix: dark active tab bg with white text

--- playground/app/layouts/auth.vue
+++ playground/app/layouts/auth.vue
@@ -62,8 +62,8 @@ const showTabs = computed(() => ['/login', '/register'].includes(route.path))
               :ui="{
                 root: 'flex flex-col gap-0 items-start',
                 list: 'p-0 rounded-none bg-transparent border-x border-t max-w-max',
-                trigger: 'justify-start rounded-none px-4 py-2 text-black dark:text-white data-[state=inactive]:opacity-40 data-[state=active]:opacity-100',
-                indicator: 'rounded-none inset-y-0 bg-white dark:bg-zinc-900/90 shadow-none',
+                trigger: 'justify-start rounded-none px-4 py-2 text-black dark:text-white data-[state=inactive]:opacity-40 data-[state=active]:text-white data-[state=active]:opacity-100',
+                indicator: 'rounded-none inset-y-0 bg-neutral-800 dark:bg-zinc-900/90 shadow-none',
               }"
             />
             <slot />
