e5ac8611e07b4eed90268600eef6d0aa06c63207
onmax <maximogarciamtnez@gmail.com>
2026-01-03 11:08:38 +0000

    fix: extend NitroRouteConfig type augmentation
    Closes #25
    
    Co-authored-by: amaury-tobias <amaury.tobiasqr@gmail.com>
    Co-authored-by: Adam Kasper <adam@obvio.cz>

--- src/module.ts
+++ src/module.ts
@@ -206,13 +206,25 @@ declare module '#nuxt-better-auth' {
     addTypeTemplate({
       filename: 'types/nuxt-better-auth-nitro.d.ts',
       getContents: () => `
+declare module 'nitropack' {
+  interface NitroRouteRules {
+    auth?: import('${resolver.resolve('./runtime/types')}').AuthMeta
+  }
+  interface NitroRouteConfig {
+    auth?: import('${resolver.resolve('./runtime/types')}').AuthMeta
+  }
+}
 declare module 'nitropack/types' {
   interface NitroRouteRules {
     auth?: import('${resolver.resolve('./runtime/types')}').AuthMeta
   }
+  interface NitroRouteConfig {
+    auth?: import('${resolver.resolve('./runtime/types')}').AuthMeta
+  }
 }
+export {}
 `,
-    })
+    }, { nuxt: true, nitro: true, node: true })
 
     // HMR
     nuxt.hook('builder:watch', async (_event, relativePath) => {
